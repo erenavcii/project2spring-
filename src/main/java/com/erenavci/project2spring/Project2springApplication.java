a865d79e02bf738a879e46ee3e7e928b9ee61e05
Eren Avcı <seningithubemailin@gmail.com>
2025-07-16 23:54:53 +0300

    Initial commit

---
+++ src/main/java/com/erenavci/project2spring/Project2springApplication.java
@@ -0,0 +1,13 @@
+package com.erenavci.project2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2springApplication.class, args);
+	}
+
+}
